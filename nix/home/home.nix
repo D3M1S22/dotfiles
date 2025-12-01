572577afcddda336cea3b2d42418f064d1ecde80
Damiano <damianoshushku@gmail.com>
2025-12-01 03:07:50 +0100

    allowing unfree app

--- nix/home/home.nix
+++ nix/home/home.nix
@@ -2,7 +2,14 @@
   home.username = "demis";
   home.homeDirectory = lib.mkForce "/Users/demis";
   # ✅ must be absolute path
-
+  
+  nixpkgs.config = {
+    # The predicate function checks if the package name is in the allowed list.
+    allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+      "code-cursor"         
+    ];
+  };
+  
   imports = [ ./module/index.nix ];
 
   home.stateVersion = "26.05";
