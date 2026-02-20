e03ceb21e60de4e6b61b046e154586460b40a13e
D3M1S22 <damianoshushku@gmail.com>
2026-02-20 11:31:23 +0100

    hyprland.conf

--- nix/home/module/themes/linux/themes.nix
+++ nix/home/module/themes/linux/themes.nix
@@ -13,5 +13,18 @@
   xdg.configFile."swayosd/theme" = lib.mkIf pkgs.stdenv.isLinux {
     source = "${dotfiles}/omarchy-style/swayosd/theme";
   };
+  
+  xdg.configFile."hypr/hyprland.conf" = lib.mkIf pkgs.stdenv.isLinux {
+    source = "${dotfiles}/omarchy-style/hypr/hyprland.conf";
+  };
+
+  xdg.configFile."hypr/hyprtheme.conf" = lib.mkIf pkgs.stdenv.isLinux {
+    text = ''
+      general {
+          col.active_border = rgba(ABA0F2ee) rgba(F28B66ee) 45deg
+          col.inactive_border = rgba(0a122088)
+      }
+    '';
+  };
 
 }
