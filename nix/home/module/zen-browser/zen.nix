697e872e6acfab10dd6df9d6625943719ddce66f
D3M1S22 <damianoshushku@gmail.com>
2026-02-25 14:01:28 +0100

    walker fixed via hyprland.conf

--- nix/home/module/zen-browser/zen.nix
+++ nix/home/module/zen-browser/zen.nix
@@ -1,24 +1,5 @@
-{ pkgs, inputs, lib, ... }:
-
-let
-  # A robust wrapper script that bypasses .desktop validation errors
-  # and ensures Wayland variables are always present even if Walker drops them.
-  zen-wrapper = pkgs.writeShellScriptBin "zen-nixgl-wrapper" ''
-    #!/usr/bin/env bash
-    
-    # If Walker/UWSM loses the display variables, fetch them directly from systemd
-    if [ -z "$WAYLAND_DISPLAY" ]; then
-      export WAYLAND_DISPLAY=$(systemctl --user show-environment | grep '^WAYLAND_DISPLAY=' | cut -d= -f2)
-    fi
-    if [ -z "$DISPLAY" ]; then
-      export DISPLAY=$(systemctl --user show-environment | grep '^DISPLAY=' | cut -d= -f2)
-    fi
-
-    # Launch Zen with nixGL and pass any arguments (like URLs) using "$@"
-    exec ${pkgs.nixgl.auto.nixGLDefault}/bin/nixGL ${inputs.zen-browser.packages."${pkgs.system}".twilight}/bin/zen "$@"
-  '';
-in
-{
+{ pkgs, inputs, lib, ... }: {
+  
   imports = [ inputs.zen-browser.homeModules.twilight ];
 
   programs.zen-browser = {
@@ -29,18 +10,13 @@ in
     };
   };
 
-  home.packages = [
-    pkgs.nixgl.auto.nixGLDefault
-    zen-wrapper
-  ];
+  home.packages = [ pkgs.nixgl.auto.nixGLDefault ];
 
   xdg.desktopEntries."zen-twilight" = {
     name = "Zen Twilight";
     genericName = "Web Browser";
-    
-    # Point directly to our clean wrapper script
-    exec = "${zen-wrapper}/bin/zen-nixgl-wrapper %u";
-    
+    # No crazy wrappers, just nixGL + Zen!
+    exec = "${pkgs.nixgl.auto.nixGLDefault}/bin/nixGL ${inputs.zen-browser.packages."${pkgs.system}".twilight}/bin/zen %u";
     icon = "zen-twilight";
     terminal = false;
     categories = [ "Network" "WebBrowser" ];
